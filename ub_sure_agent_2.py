9744ce8f2388bb8a21239bba8cacf4bd38c7cbb7
Victor Aynbinder <>
2025-05-27 15:03:37 +0300

    no message

--- ub_sure_agent_2.py
+++ ub_sure_agent_2.py
@@ -134,7 +134,7 @@ retriever = SelfQueryRetriever.from_llm(
     # metadata_field_info = metadata_field_info,
     # examples = filter_examples_simplified,
     verbose=True,
-    search_kwargs= {"k": 7},
+    search_kwargs= {"k": 6},
 )
 
 
